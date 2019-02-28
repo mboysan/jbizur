81364330856f9acb22b302696156f83f3f083eec
mboysan <mboysan.git@gmail.com>
2019-02-28 19:47:45 +0200

    refs #40 - travis script to upload logs (4)

--- scripts/uploadlog.sh
+++ scripts/uploadlog.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl --upload-file ../config/log/log*.txt https://transfer.sh/log.txt
+cd ../config/log
+LOGFILE="$(ls -t log* | head -1)"
+echo "uploading ${LOGFILE} ..."
+curl -s --upload-file $LOGFILE https://transfer.sh/log.txt
