fe98aba0b20cdf2385b63790c45f8b4c218f7b80
mboysan <mboysan.git@gmail.com>
2019-02-28 20:08:55 +0200

    refs #40 - travis script to upload logs (6)

--- scripts/uploadlog.sh
+++ scripts/uploadlog.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ../config/log
+echo "switching to log dir: ${1}"
+cd $1
 LOGFILE="$(ls -t log* | head -1)"
 echo "uploading ${LOGFILE} ..."
 curl -s --upload-file $LOGFILE https://transfer.sh/log.txt | tee out.txt
