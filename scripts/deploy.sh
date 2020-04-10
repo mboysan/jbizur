68961473eff05dfbcf99e461670778a22a360817
mboysan <mboysan.git@gmail.com>
2020-04-10 21:09:58 +0300

    test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,20 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "mboysan.git@gmail.com"
-  git config --global user.name "mboysan"
-}
+# setup git
+git config --global user.email "mboysan.git@gmail.com"
+git config --global user.name "mboysan"
 
-get_branch() {
-  cd $HOME
-  git clone --depth=1 --branch=repository https://github.com/mboysan/jbizur.git jbizur-repository
-  cd jbizur-repository
-}
+# get branch
+cd $HOME
+git clone --depth=1 --branch=repository https://github.com/mboysan/jbizur.git jbizur-repository
+cd jbizur-repository
 
-commit_files() {
-  cp -avr $HOME/.m2/repository/ee/ut/jbizur/* ./ee/ut/jbizur/
-  git add --all
-  git commit --message "Travis build jbizur: $TRAVIS_BUILD_NUMBER"
-}
+# commit files
+cp -avr $HOME/.m2/repository/ee/ut/jbizur/* ./ee/ut/jbizur/
+git add --all
+git commit --message "Travis build jbizur: $TRAVIS_BUILD_NUMBER"
 
-upload_files() {
-  git remote rm origin
-  git remote add origin https://${GITHUB_TOKEN}@github.com/mboysan/jbizur.git
-  git push --quiet origin repository
-}
-
-setup_git
-get_branch
-commit_files
-upload_files
+# upload files
+git remote rm origin
+git remote add origin https://${GITHUB_TOKEN}@github.com/mboysan/jbizur.git
+git push --quiet origin repository
