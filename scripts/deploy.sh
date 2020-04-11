b32d68cdc2583f40aecd10afede207cc76e99ae7
mboysan <mboysan.git@gmail.com>
2020-04-11 16:08:30 +0300

    test deploy (5) - working on flatten plugin

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+PROJ_VERSION=$(mvn -q \
+    -Dexec.executable=echo \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    exec:exec)
+
+export PROJ_VERSION
+
 # setup git
 git config --global user.email "mboysan.git@gmail.com"
 git config --global user.name "mboysan"
@@ -21,3 +29,7 @@ git commit --message "Travis build jbizur: $TRAVIS_BUILD_NUMBER"
 git remote rm origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/mboysan/jbizur.git
 git push --quiet origin repository
+
+# tag and push
+git tag v$PROJ_VERSION.$TRAVIS_BUILD_NUMBER
+git push origin --tags
